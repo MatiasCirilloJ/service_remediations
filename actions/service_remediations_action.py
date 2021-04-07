d3e8aa1a6fb64c09135f9d792501311e79a0969b
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-04-07 11:21:36 -0300

    Update service_remediations_action.py

--- actions/service_remediations_action.py
+++ actions/service_remediations_action.py
@@ -5,4 +5,4 @@ from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
     def run(self, queue):
-        return (os.system("st2 run core.remote hosts='10.54.158.195' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /root ; python rabbitmqadmin purge queue name={}'".format(queue)))
+        return (os.system("st2 run core.remote hosts='10.54.158.195' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /root ; ansible-playbook ans-playbook.yaml))
