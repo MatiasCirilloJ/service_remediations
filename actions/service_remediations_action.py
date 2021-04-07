f0570cb165648478dc8a5ff186db98816cdca927
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-04-07 17:27:15 -0300

    Update service_remediations_action.py

--- actions/service_remediations_action.py
+++ actions/service_remediations_action.py
@@ -6,15 +6,17 @@ from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
     def run(self, message, id="", idTag="", levelTag="", messageField="", durationField=""):
-        try:
-            
-            with open("/opt/stackstorm/packs/service_remediations_pack/actions/logs.txt", "a") as f:
-                f.write(message + "\n")
-            
+        service = message.split()[0]
+        value = int(message[-1])
+        if service == "NEP@L_Monitoring" and value != 0:
             os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose stop && docker-compose up -d)
             time.sleep(30)
             os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && systemctl stop nodeserver && systemctl start nodeserver)
-            return (True, "Success")
+        try:
+            with open("/opt/stackstorm/packs/service_remediations_pack/actions/logs.txt", "a") as f:
+                f.write(message + "\n")
         except IOError:
             return (False, "File not accessible")
+        
+        return (True, "Success")
         #os.system("st2 run core.remote hosts='10.54.158.243' username='root' password='Cordob12' cmd='cd /root ; echo $HOME")
