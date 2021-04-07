05531868cb2eb2e9d9f07683d347a3d4f10168eb
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-04-07 15:48:00 -0300

    Update service_remediations_action.py

--- actions/service_remediations_action.py
+++ actions/service_remediations_action.py
@@ -4,5 +4,11 @@ import os
 from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
-    def run(self, service):
-        return (os.system("st2 run core.remote hosts='10.54.158.195' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /root ; ansible-playbook ans-playbook.yaml"))
+    def run(self, message):
+        try:
+            with open("logs.txt", "a") as f:
+                f.write(message)
+                return (True, "Success")
+        except IOError:
+            return (False, "File not accessible")
+        #os.system("st2 run core.remote hosts='10.54.158.243' username='root' password='Cordob12' cmd='cd /root ; echo $HOME")
