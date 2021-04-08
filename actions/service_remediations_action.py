0e439122819cd6514d9b0bea242930ea47ed4288
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-04-08 12:49:29 -0300

    v0.1.9 - I/O rule (fixed)

--- actions/service_remediations_action.py
+++ actions/service_remediations_action.py
@@ -14,9 +14,9 @@ class MyEchoAction(Action):
             service = message.split()[0]
             value = int(message[-1])
             if service == "NEP@L_Controller" and value != 0:
-                os.system("st2 rule disable remediations_pack.service_remediations_rule")
-                time.sleep(30)
-                os.system("st2 rule enable remediations_pack.service_remediations_rule")
+                os.system("st2 rule disable service_remediations_pack.service_remediations_rule")
+                time.sleep(15)
+                os.system("st2 rule enable service_remediations_pack.service_remediations_rule")
                 logging.basicConfig(filename='/opt/stackstorm/packs/service_remediations_pack/actions/action.log', format='%(name)s - %(levelname)s - %(message)s')
                 logging.error(message)
                 #os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose stop)
