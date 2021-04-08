f4216ccf846720a752f0ac32536617de718469a5
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-04-08 11:16:05 -0300

    v0.1.3

--- actions/service_remediations_action.py
+++ actions/service_remediations_action.py
@@ -14,8 +14,10 @@ class MyEchoAction(Action):
             service = message.split()[0]
             value = int(message[-1])
             if service == "NEP@L_Controller" and value != 0:
-                logging.basicConfig(filename='/opt/stackstorm/packs/service_remediations_pack/actions/action.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-                logging.error('Testeo de logging')
+                logging.basicConfig(filename='/opt/stackstorm/packs/service_remediations_pack/actions/action.log', format='%(name)s - %(levelname)s - %(message)s')
+                logging.error(message)
+                time.sleep(30)
+                logging.debug("listo")
                 #os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose stop)
                 #time.sleep(30)
                 #os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose up -d)
