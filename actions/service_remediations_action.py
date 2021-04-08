7b6c4c56ffd7c6782842f72c1330bbe5e0987cbb
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-04-08 10:49:23 -0300

    add logging

--- actions/service_remediations_action.py
+++ actions/service_remediations_action.py
@@ -1,24 +1,31 @@
 import sys
 import os
 import time
+import logging
 
 from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
     def run(self, message, id="", idTag="", levelTag="", messageField="", durationField=""):
-        service = message.split()[0]
-        value = int(message[-1])
-        if service == "NEP@L_Controller" and value != 0:
-            os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose stop)
-            time.sleep(30)
-            os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose up -d)
-            time.sleep(30)
-            os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && systemctl stop nodeserver && systemctl start nodeserver)
+        
         try:
             with open("/opt/stackstorm/packs/service_remediations_pack/actions/logs.txt", "a") as f:
+                print("LLEGÓ A 2")
                 f.write(message + "\n")
+            service = message.split()[0]
+            value = int(message[-1])
+            if service == "NEP@L_Controller" and value != 0:
+                logging.basicConfig(filename='action.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+                logging.error('Testeo de logging')
+                #os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose stop)
+                #time.sleep(30)
+                #os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose up -d)
+                #time.sleep(30)
+                #os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && systemctl stop nodeserver && systemctl start nodeserver)
+            return (True, "Success")
         except IOError:
             return (False, "File not accessible")
+        except:
+            return (False)
         
-        return (True, "Success")
         #os.system("st2 run core.remote hosts='10.54.158.243' username='root' password='Cordob12' cmd='cd /root ; echo $HOME")
