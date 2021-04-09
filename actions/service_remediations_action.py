f2f8cd6056fa68226d2e2568f1ede5daacb8e4a2
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-04-09 11:16:07 -0300

    v0.1.12

--- actions/service_remediations_action.py
+++ actions/service_remediations_action.py
@@ -1,31 +1,38 @@
 import sys
 import os
 import time
-import logging
 
 from st2common.runners.base_action import Action
+
+remote = "st2 run core.remote "
 
 class ServiceRemediationsAction(Action):
     def run(self, message, id=None, idTag=None, levelTag=None, messageField=None, durationField=None):
         try:
             with open("/opt/stackstorm/packs/service_remediations_pack/actions/logs.txt", "a") as f:
                 f.write(message + "\n")
+
+            IO_rule = "st2 rule {} service_remediations_pack.service_remediations_rule"
+            remote = "st2 run core.remote hosts={} username={} private_key={} cmd={}"
+            with open('/opt/stackstorm/packs/service_remediations_pack/actions/service_data.json') as file:
+                service_data = json.load(file)
+
             service = message.split()[0]
             value = int(message[-1])
-            if service == "NEP@L_Controller" and value != 0:
-                os.system("st2 rule disable service_remediations_pack.service_remediations_rule")    #Disable webhook rule
-                os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose stop'") 
-                time.sleep(30)
-                os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && docker-compose up -d'")
-                time.sleep(30)
-                os.system("st2 run core.remote hosts='10.54.158.194' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /opt/django-nepal-be && systemctl stop nodeserver && systemctl start nodeserver'")
-                os.system("st2 rule enable service_remediations_pack.service_remediations_rule")    #Enable webhook rule
-                #logging.basicConfig(filename='/opt/stackstorm/packs/service_remediations_pack/actions/action.log', format='%(name)s - %(levelname)s - %(message)s')
-                #logging.error(message)
+
+            if service in service_data:
+                remote_with_service = remote.format(service_data[service]['host'], service_data[service]['username'], service_data[service]['private_key'], '{}')
+                os.system(IO_rule.format('disable'))    #Disable webhook rule
+                if service == "NEP@L_Controller" and value != 0:
+                    os.system(controller.format(service_data[service]['cmd']['stop']))
+                    time.sleep(20)
+                    os.system(controller.format(service_data[service]['cmd']['up']))
+                    time.sleep(20)
+                    os.system(controller.format(service_data[service]['cmd']['systemctl']))
+                os.system(IO_rule.format('enable'))    #Enable webhook rule
             return (True, "Success")
+
         except IOError:
             return (False, "File not accessible")
         except:
             return (False)
-        
-        #os.system("st2 run core.remote hosts='10.54.158.243' username='root' password='Cordob12' cmd='cd /root ; echo $HOME")
