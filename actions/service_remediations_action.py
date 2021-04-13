9dc58067b5fced36eec6ba0ad7e17769d0d15215
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-04-13 09:46:54 -0300

    v0.2.9 "new model"

--- actions/service_remediations_action.py
+++ actions/service_remediations_action.py
@@ -5,7 +5,14 @@ import json
 
 from st2common.runners.base_action import Action
 
-remote = "st2 run core.remote "
+def send_command(service, service_data):
+    remote_with_service = remote.format(service_data[service]['host'], service_data[service]['username'], service_data[service]['private_key'], '{}')
+    os.system(io_rule.format('disable'))    #Disable webhook rule
+    for cmd in service_data[service]['cmd']:
+        #os.system(remote_with_service.format(service_data[service]['cmd'][cmd]))
+        print(remote_with_service.format(service_data[service]['cmd'][cmd]))
+        time.sleep(20)
+    os.system(io_rule.format('enable'))    #Enable webhook rule
 
 class ServiceRemediationsAction(Action):
     def run(self, message, id=None, idTag=None, levelTag=None, messageField=None, durationField=None):
@@ -13,33 +20,16 @@ class ServiceRemediationsAction(Action):
             with open("/opt/stackstorm/packs/service_remediations_pack/actions/logs.txt", "a") as f:
                 f.write(message + "\n")
 
-            IO_rule = "st2 rule {} service_remediations_pack.service_remediations_rule"
-            remote = "st2 run core.remote hosts={} username={} private_key={} cmd={}"
             with open('/opt/stackstorm/packs/service_remediations_pack/actions/service_data.json') as file:
                 service_data = json.load(file)
+            io_rule = service_data['Commands']['IO_rule']
+            remote = service_data['Commands']['remote']
 
             service = message.split()[0]
             value = int(message[-1])
 
-            remote_with_service = remote.format(service_data[service]['host'], service_data[service]['username'], service_data[service]['private_key'], '{}')
-                
-            if service == "NEP@L_Controller" and value != 0:
-                os.system(IO_rule.format('disable'))    #Disable webhook rule
-                os.system(remote_with_service.format(service_data[service]['cmd']['stop']))
-                time.sleep(20)
-                os.system(remote_with_service.format(service_data[service]['cmd']['up']))
-                time.sleep(20)
-                os.system(remote_with_service.format(service_data[service]['cmd']['systemctl']))
-                os.system(IO_rule.format('enable'))    #Enable webhook rule
-            elif service == "NEP@L_SysLog" and value != 0:
-                os.system(IO_rule.format('disable'))    #Disable webhook rule
-                os.system(remote_with_service.format(service_data[service]['cmd']['stop']))
-                time.sleep(30)
-                os.system(remote_with_service.format(service_data[service]['cmd']['up']))
-                time.sleep(60)
-                os.system(IO_rule.format('enable'))    #Enable webhook rule
-            else:
-                pass
+            if service in service_data and value != 0:
+                send_command(service, service_data)
 
             return (True, "Success")
 
