3c06045b92fefd098c21d6f5aaed087da3a4377f
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-04-27 14:04:41 -0300

    Global variables in monitoring remediation

--- actions/monitoring_remediation.py
+++ actions/monitoring_remediation.py
@@ -2,14 +2,16 @@ import requests
 import time
 from os import system
 
+url = "http://10.54.158.207:4348/containers/json?all=l"
+command = "st2 action execute service_remediations_pack.service_remediations_action message='NEP@L_Monitoring 1'"
+
 def ejecutaScript():
-    url = "http://10.54.158.207:4348/containers/json?all=l"
     res = requests.get(url)
     js = res.json() 
 
     for service in js:
         if str(service["Names"]) == "['/kapacitor']" and str(service["State"]) != "running":
-            system("st2 action execute service_remediations_pack.service_remediations_action message='NEP@L_Monitoring 1'")
+            system(command)
 while True:
     ejecutaScript()
     time.sleep(300)
