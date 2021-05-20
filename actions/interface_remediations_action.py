f5aa0e8fb29ab688b5c6b1a841560271344eb65f
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-05-20 16:37:04 -0300

    add interface remed

---
+++ actions/interface_remediations_action.py
@@ -0,0 +1,27 @@
+import json
+from functions import send_service_command
+from datetime import datetime
+from pytz import timezone
+tz = timezone("America/Buenos_Aires")
+
+from st2common.runners.base_action import Action
+
+class InterfaceRemediationsAction(Action):
+    def run(self, message, id=None, idTag=None, levelTag=None, messageField=None, durationField=None):
+        try:
+            #with open('/opt/stackstorm/packs/service_remediations_pack/actions/service_data.json') as file:
+            #    service_data = json.load(file)
+            #io_rule = service_data['Commands']['IO_rule']["service"]
+            #remote = service_data['Commands']['remote']
+            
+            #host = message.split()[0]
+            #service = message.split()[2]
+
+            if message:
+                with open("/opt/stackstorm/packs/service_remediations_pack/actions/logs.txt", "a") as f:
+                    f.write("{} | {}\n".format(tz.localize(datetime.now()).strftime("%D-%H:%M:%S"), message))
+
+            return (False, "Message doesn't match")
+        
+        except IOError:
+            return (False, "File not accessible")
