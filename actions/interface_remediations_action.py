e38c031b293bf6664455a202c5075438216bc007
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-06-08 16:14:35 -0300

    Update interface_remediations_action.py

--- actions/interface_remediations_action.py
+++ actions/interface_remediations_action.py
@@ -1,4 +1,5 @@
 import re
+from time import sleep
 from functions import send_interface_command, syslog
 from datetime import datetime
 from pytz import timezone
@@ -15,6 +16,7 @@ class InterfaceRemediationsAction(Action):
                 with open("/opt/stackstorm/packs/service_remediations_pack/actions/logs.txt", "a") as f:
                     f.write("{} | {}, {}\n".format(tz.localize(datetime.now()).strftime("%D-%H:%M:%S"), message,host))
                 
+                sleep(10)
                 ans_stat = send_interface_command(interface, host)
                 status = re.sub('\s+',' ', ans_stat.split('\n')[2]).split()[3]
                 syslog("Interface", host, message, "'no shut'", status)
